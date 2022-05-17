b0a5b45defad258eb1fc481aad924cf770763011
BJ Anderson <andersonb79@gmail.com>
2022-05-17 15:10:28 -0400

    added time formatter

--- src/utils/formatters/formatters.ts
+++ src/utils/formatters/formatters.ts
@@ -18,3 +18,24 @@ export function numToDollars(num: number): string {
 export function dollarsToNum(dollars: string): number {
   return getNumber(parseFloat(getString(dollars).replace(/[$,\s]/g, '')));
 }
+
+export function msToTime(milliSeconds: number): string {
+  let time = Math.floor(milliSeconds / 1000);
+  const s = time % 60;
+  time = Math.floor(time / 60);
+  const m = time % 60;
+  time = Math.floor(time / 60);
+  const h = time % 24;
+  time = Math.floor(time / 24);
+  const d = time;
+  if (d) {
+    return `${d}d ${h}h`;
+  }
+  if (h) {
+    return `${h}h ${m}m`;
+  }
+  if (m) {
+    return `${m}m ${s}s`;
+  }
+  return `${s}s`;
+}
