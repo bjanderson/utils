ee035a7091b51026a86274343d19d580c05af758
BJ Anderson <andersonb79@gmail.com>
2022-05-17 14:10:37 -0400

    added formatters to barrel

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,7 @@
 export * from './array';
 export * from './date';
 export * from './default-values';
+export * from './formatters';
 export * from './model';
 export * from './primitive';
 export * from './string';
